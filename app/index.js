2b79e28a762b7115e1288d09cfc82798395dd36f
Valerie Burzynski <vburzynski@shutterstock.com>
2017-02-07 23:26:10 -0600

    npm scripts

--- app/index.js
+++ app/index.js
@@ -1,2 +1,9 @@
-var body = document.getElementsByTagName("body");
-body.innerHTML += "<p>Hello World</p>";
+import _ from 'lodash';
+
+function component () {
+  var element = document.createElement('div');
+  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  return element;
+}
+
+document.body.appendChild(component());

959ef9dadf337a8cbbdfa56a854a9f0ee6fe5afa
Valerie Burzynski <vburzynski@shutterstock.com>
2017-02-08 10:55:19 -0600

    Adds babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
